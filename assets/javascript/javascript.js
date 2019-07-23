9690fe060ab16760bd5ea1fdd5e6221ec24ae78a
Sam Pai <sampai@Sams-MacBook-Pro.local>
2019-07-22 21:25:42 -0700

    added variables

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,7 +6,18 @@ function search() {
 // Get Search Values
 // Build Request
     // then Update UI with articles
-};
+    $("#search").on("click", function() {
+        var searchTerm = $("#search").text();
+        var numOfRecords = $("#numRecords").text();
+        var startYear = $("#startYear").text();
+        var endYear = $("#endYear").text();
+
+        var termUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=xacbx8TjHAqg468sAprP4vmPH7gxuIQT&q=subject:("' + searchTerm + '")';
+        console.log(termUrl)
+        var recordsUrl = 
+        var startUrl
+        var endUrl
+});
 
 function clearResults() {
     $("#articlesList").empty();
