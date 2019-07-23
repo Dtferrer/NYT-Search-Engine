5b94e7a742509760ea5fe14c6a3e1e5b721eae09
Sam Pai <sampai@Sams-MacBook-Pro.local>
2019-07-22 20:38:57 -0700

    updated framework

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
 });
 
 function search() {
-
+// Get Search Values
+// Build Request
+    // then Update UI with articles
 };
 
 function clearResults() {
-
+// Empty article
 };
 
